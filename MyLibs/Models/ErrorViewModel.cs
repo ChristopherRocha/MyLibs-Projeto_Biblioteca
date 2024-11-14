bd3a03a6adcd2b4f301b1a29cc9eac0a751bba92
Christopher Carpegiane <christophercarpegiane@gmail.com>
2024-11-14 13:13:31 -0300

    Create Project

---
+++ MyLibs/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyLibs.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
